7446c2570bb511aaa678dd6d833798b871c77c23
Kalpesh Bonde <kalpeshbonde04@gmail.com>
2025-07-01 22:48:08 +0530

    Forntend work

--- clientside/tailwind.config.js
+++ clientside/tailwind.config.js
@@ -4,7 +4,7 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: "#5f6FFF",
+        primary: "#60B5FF",
       },
       gridTemplateColumns: {
         auto: "repeat(auto-fill, minmax(200px, 1fr))",
